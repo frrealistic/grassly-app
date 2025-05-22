006f750248040c523b5d5093594cbbf92855daa8
fiskultura <142538271+fiskultura@users.noreply.github.com>
2025-05-23 00:53:26 +0200

    database update

--- backend/db.js
+++ backend/db.js
@@ -16,8 +16,44 @@ function initDatabase() {
                     name TEXT NOT NULL,
                     email TEXT UNIQUE NOT NULL,
                     password TEXT NOT NULL,
+                    isadmin INTEGER DEFAULT 0, -- 0 = nije admin, 1 = admin
                     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
         )`);
+        db.run(`CREATE TABLE IF NOT EXISTS fields (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    userId INTEGER NOT NULL,
+                    name TEXT NOT NULL,
+                    location TEXT,
+                    area REAL,
+                    lat REAL,
+                    lng REAL,
+                    topLeftLat REAL,
+                    topLeftLng REAL,
+                    bottomRightLat REAL,
+                    bottomRightLng REAL,
+                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
+        )`);
+        db.run(`CREATE TABLE IF NOT EXISTS probes (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    fieldId INTEGER NOT NULL,
+                    lat REAL,
+                    lng REAL,
+                    description TEXT,
+                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    FOREIGN KEY (fieldId) REFERENCES fields(id) ON DELETE CASCADE
+                )`)
+        db.run(`CREATE TABLE IF NOT EXISTS measurements (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    probeId INTEGER NOT NULL,
+                    moisture REAL,
+                    temperature REAL,
+                    ec REAL,
+                    measured_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    FOREIGN KEY (probeId) REFERENCES probes(id) ON DELETE CASCADE
+                )`)
+
+
     });
 }
 
